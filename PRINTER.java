42365bf7cc9868d22bedf7776cc144b710fe79f1
ItzMythhh <165608097+ItzMythhh@users.noreply.github.com>
2026-02-02 12:10:49 -0500

    Implement customer data printer from file
    This program reads customer data from a file and prints it in a formatted manner.

---
+++ PRINTER.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class PRINTER {
+    public static void main(String[] args) throws Exception {
+
+        Scanner input = new Scanner(new File("customers.txt"));
+        input.useDelimiter("[,\\s\\n]+");
+
+        System.out.println("---------------------------------------");
+        System.out.println("ID Name Balance");
+        System.out.println("---------------------------------------");
+
+        while (input.hasNext()) {
+            int id = input.nextInt();
+            String name = input.next();
+            double balance = input.nextDouble();
+
+            String idText = "" + id;
+            while (idText.length() < 6) {
+                idText = "0" + idText;
+            }
+
+            System.out.println(idText + " " + name + " $" + balance);
+        }
+
+        input.close();
+    }
+}
