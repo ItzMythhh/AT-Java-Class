69e95d11ab3fa372601981cd3e7cc786e3a3a0f3
ItzMythhh <165608097+ItzMythhh@users.noreply.github.com>
2026-03-02 18:02:55 -0500

    Add MATH class to calculate area and square root

---
+++ MATH.java
@@ -0,0 +1,10 @@
+public class MATH {
+    public static void main(String[] args) {
+
+        double area = Math.PI * 2 * 2;
+        System.out.println(area);
+
+        double root = Math.sqrt(16);
+        System.out.println(root);
+    }
+}
