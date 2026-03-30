9480a0d3881fabe676a08bab42fda990e01fce8b
ItzMythhh <165608097+ItzMythhh@users.noreply.github.com>
2026-03-30 17:44:11 -0400

    Create Manager.java

---
+++ Manager.java
@@ -0,0 +1,16 @@
+public class Manager extends Employee
+{
+    private double bonus;
+
+    public Manager(String name, double salary, double bonus)
+    {
+        super(name, salary);
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double getAnnualIncome()
+    {
+        return getSalary() + bonus;
+    }
+}
