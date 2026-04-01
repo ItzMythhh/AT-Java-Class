beb291dc0c3da02f247a5e6d9c2604e8d5c2b405
ItzMythhh <165608097+ItzMythhh@users.noreply.github.com>
2026-04-01 10:23:46 -0400

    Refactor Yard class constructor and methods
    Refactor constructor and methods for better clarity and maintainability.

--- Mow/Yard.java
+++ Mow/Yard.java
@@ -12,25 +12,19 @@ public class Yard {
     private char unmowed = '+';
     private char mowed = ' ';
 
-    // constructor for lawn, fills the yard array
     public Yard(int numrows, int numcolums) {
         length = numrows + 2;
         width = numcolums + 2;
         yard = new char[length][width];
-        // traverse the array.
         for (int row = 0; row < yard.length; row++) {
             for (int col = 0; col < yard[0].length; col++) {
                 if (row == 0) {
-                    // if first row make it a border
                     yard[row][col] = border;
                 } else if (row == yard.length - 1) {
-                    // if last row make it a border
                     yard[row][col] = border;
                 } else if (col == 0) {
-                    // if first colum make it a border
                     yard[row][col] = border;
                 } else if (col == yard[0].length - 1) {
-                    // if second colum make it a border
                     yard[row][col] = border;
                 } else {
                     yard[row][col] = unmowed;
@@ -39,30 +33,25 @@ public class Yard {
         }
     }
 
-    // returns the character (lawn square)
     public char getLawnSquare(int row, int colum) {
         return yard[row][colum];
     }
 
-    // this method allows you to overwrite an element of the array
     public char setLawnSquare(int row, int colum, char newSquare) {
         yard[row][colum] = newSquare;
         return yard[row][colum];
     }
 
-    // method that returns height of lawn
     public int getHeight() {
         int height = yard.length - 2;
         return height;
     }
 
-    // get width of the lawn
     public int getWidth() {
         int width = yard[0].length - 2;
         return width;
     }
 
-    // method to print the lawn
     public void printLawn(Mower inputMower) {
         for (int row = 0; row < yard.length; row++) {
             for (int col = 0; col < yard[0].length; col++) {
