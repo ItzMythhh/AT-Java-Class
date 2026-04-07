aa581f82fe1df3a19defa563c6889e407fd42d6a
ItzMythhh <165608097+ItzMythhh@users.noreply.github.com>
2026-04-07 12:37:00 -0400

    Update Demo.java

--- Mow/Demo.java
+++ Mow/Demo.java
@@ -1,15 +1,41 @@
-Mower mower = new Mower();
-mower.setVerticalPos(1);
-mower.setHorizontalPos(1);
-mower.setDirection(1); // facing right
+import java.util.Scanner;
+import Mow.Yard;
+import Mow.Mower;
 
-while (mower.getHorizontalPos() < yard.getWidth()) {
-    Yard.clearScreen();
+public class YardDemo {
 
-    mower.mowSpace(yard);
-    yard.printLawn(mower);
+    public static void delay(long mseconds) {
+        try {
+            Thread.sleep(mseconds);
+        } catch (InterruptedException e) {
+            System.err.println("InterruptedException received!");
+        }
+    }
 
-    delay(500);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    mower.moveMower();
+        System.out.print("Enter the height of the yard: ");
+        int height = input.nextInt();
+
+        System.out.print("Enter the width of the yard: ");
+        int width = input.nextInt();
+
+        Yard yard = new Yard(height, width);
+
+        Mower mower = new Mower();
+        mower.setVerticalPos(1);
+        mower.setHorizontalPos(1);
+        mower.setDirection(1);
+
+        while (mower.getHorizontalPos() <= yard.getWidth()) {
+            Yard.clearScreen();
+            mower.mowSpace(yard);
+            yard.printLawn(mower);
+            delay(500);
+            mower.moveMower();
+        }
+
+        input.close();
+    }
 }
