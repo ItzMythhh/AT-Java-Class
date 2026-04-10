e155f0599b0f774b9827b3bd93fd9976245c9aa9
ItzMythhh <165608097+ItzMythhh@users.noreply.github.com>
2026-04-09 20:22:24 -0400

    Update Mower.java

--- Mow/Mower.java
+++ Mow/Mower.java
@@ -2,40 +2,35 @@ package Mow;
 
 import java.util.Random;
 
-import Mow.Yard;
-
 public class Mower {
 
     private int verticalPosition;
     private int horizontalPosition;
     private int direction;
 
-    public int getHorizontalPos() {
-        return horizontalPosition;
-    }
-
     public int getVerticalPos() {
         return verticalPosition;
     }
 
-    public void setHorizontalPos(int inputcol) {
-        horizontalPosition = inputcol;
+    public int getHorizontalPos() {
+        return horizontalPosition;
     }
 
-    public void setVerticalPos(int inputrow) {
-        verticalPosition = inputrow;
+    public void setVerticalPos(int row) {
+        verticalPosition = row;
+    }
+
+    public void setHorizontalPos(int col) {
+        horizontalPosition = col;
     }
 
     public int getDirection() {
         return direction;
     }
 
-    public void setDirection(int inputdirection) {
-
-        if (0 <= inputdirection && inputdirection < 3) {
-            direction = inputdirection;
-        } else {
-            System.err.println("Direction entered is not a number between 0 and 3. Please enter a new direction.");
+    public void setDirection(int newDirection) {
+        if (newDirection >= 0 && newDirection <= 3) {
+            direction = newDirection;
         }
     }
 
@@ -55,98 +50,90 @@ public class Mower {
         direction--;
         if (direction < 0) {
             direction = 3;
-        } else if (direction > 3) {
-            direction = 0;
         }
     }
 
     public void turnRight() {
         direction++;
-        if (direction < 0) {
-            direction = 4;
-        } else if (direction > 3) {
+        if (direction > 3) {
             direction = 0;
         }
     }
 
-    public void mowSpace(Yard currentYard) {
-        currentYard.setLawnSquare(getVerticalPos(), getHorizontalPos(), ' ');
+    public void mowSpace(Yard yard) {
+        yard.setLawnSquare(verticalPosition, horizontalPosition, ' ');
     }
 
-    public boolean checkSquare(Yard inputYard) {
-        if (direction == 0 && (inputYard.getLawnSquare(getVerticalPos() - 1, getHorizontalPos()) == 'R'
-                || inputYard.getLawnSquare(getVerticalPos() - 1, getHorizontalPos()) == ' ')) {
-            return true;
-        } else if (direction == 1 && (inputYard.getLawnSquare(getVerticalPos(), getHorizontalPos() + 1) == 'R'
-                || inputYard.getLawnSquare(getVerticalPos(), getHorizontalPos() + 1) == ' ')) {
-            return true;
-        } else if (direction == 2 && (inputYard.getLawnSquare(getVerticalPos() + 1, getHorizontalPos()) == 'R'
-                || inputYard.getLawnSquare(getVerticalPos() + 1, getHorizontalPos()) == ' ')) {
-            return true;
-        } else if (direction == 3 && (inputYard.getLawnSquare(getVerticalPos(), getHorizontalPos() - 1) == 'R'
-                || inputYard.getLawnSquare(getVerticalPos(), getHorizontalPos() - 1) == ' ')) {
-            return true;
-        }
-        return false;
-    }
+    public void randomize(Yard yard) {
+        Random random = new Random();
+        int corner = random.nextInt(4);
+        direction = random.nextInt(4);
 
-    public void randomize(Yard inputYard) {
-        int yardwidth = inputYard.getWidth();
-        int yardheight = inputYard.getHeight();
-
-        Random rand = new Random();
-        int corner = rand.nextInt(4) + 1;
-
-        if (corner == 1) {
-            setHorizontalPos(1);
-            setVerticalPos(yardheight);
-        } else if (corner == 2) { 
-            setHorizontalPos(1);
-            setVerticalPos(1);
-        } else if (corner == 3) {
-            setHorizontalPos(yardwidth);
-            setVerticalPos(yardheight);
-        } else { 
-            setHorizontalPos(yardwidth);
-            setVerticalPos(1);
-        }
-
-        direction = rand.nextInt(4);
-    }
-
-    public boolean updateLeftMower(Yard inputYard) {
-        char squareLeft;
-        if (direction == 0) {
-            squareLeft = inputYard.getLawnSquare(verticalPosition, horizontalPosition - 1);
-        } else if (direction == 1) {
-            squareLeft = inputYard.getLawnSquare(verticalPosition - 1, horizontalPosition);
-        } else if (direction == 2) {
-            squareLeft = inputYard.getLawnSquare(verticalPosition, horizontalPosition + 1);
+        if (corner == 0) {
+            verticalPosition = 1;
+            horizontalPosition = 1;
+        } else if (corner == 1) {
+            verticalPosition = 1;
+            horizontalPosition = yard.getWidth();
+        } else if (corner == 2) {
+            verticalPosition = yard.getHeight();
+            horizontalPosition = 1;
         } else {
-            squareLeft = inputYard.getLawnSquare(verticalPosition + 1, horizontalPosition);
+            verticalPosition = yard.getHeight();
+            horizontalPosition = yard.getWidth();
         }
-        if (squareLeft == '+') {
+    }
+
+    public boolean updateMower(Yard yard) {
+        if (frontIsGrass(yard)) {
+            moveMower();
+            return true;
+        } else if (rightIsGrass(yard)) {
+            turnRight();
+            moveMower();
+            return true;
+        } else if (leftIsGrass(yard)) {
+            turnLeft();
+            moveMower();
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean updateRightMower(Yard inputYard) {
-        char squareRight;
+    public boolean frontIsGrass(Yard yard) {
         if (direction == 0) {
-            squareRight = inputYard.getLawnSquare(verticalPosition, horizontalPosition + 1);
+            return yard.getLawnSquare(verticalPosition - 1, horizontalPosition) == '+';
         } else if (direction == 1) {
-            squareRight = inputYard.getLawnSquare(verticalPosition + 1, horizontalPosition);
+            return yard.getLawnSquare(verticalPosition, horizontalPosition + 1) == '+';
         } else if (direction == 2) {
-            squareRight = inputYard.getLawnSquare(verticalPosition, horizontalPosition - 1);
+            return yard.getLawnSquare(verticalPosition + 1, horizontalPosition) == '+';
         } else {
-            squareRight = inputYard.getLawnSquare(verticalPosition - 1, horizontalPosition);
+            return yard.getLawnSquare(verticalPosition, horizontalPosition - 1) == '+';
         }
-        if (squareRight == '+') {
-            return true;
+    }
+
+    public boolean rightIsGrass(Yard yard) {
+        if (direction == 0) {
+            return yard.getLawnSquare(verticalPosition, horizontalPosition + 1) == '+';
+        } else if (direction == 1) {
+            return yard.getLawnSquare(verticalPosition + 1, horizontalPosition) == '+';
+        } else if (direction == 2) {
+            return yard.getLawnSquare(verticalPosition, horizontalPosition - 1) == '+';
         } else {
-            return false;
+            return yard.getLawnSquare(verticalPosition - 1, horizontalPosition) == '+';
+        }
+    }
+
+    public boolean leftIsGrass(Yard yard) {
+        if (direction == 0) {
+            return yard.getLawnSquare(verticalPosition, horizontalPosition - 1) == '+';
+        } else if (direction == 1) {
+            return yard.getLawnSquare(verticalPosition - 1, horizontalPosition) == '+';
+        } else if (direction == 2) {
+            return yard.getLawnSquare(verticalPosition, horizontalPosition + 1) == '+';
+        } else {
+            return yard.getLawnSquare(verticalPosition + 1, horizontalPosition) == '+';
         }
     }
 }
